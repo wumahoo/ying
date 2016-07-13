bdf85e7a3f72aee17f1dfbd3959d7953f27de120
wumahoo <471155350@qq.com>
2016-07-14 00:14:09 +0800

    use adminLTE view templates

--- common/config/main.php
+++ common/config/main.php
@@ -5,12 +5,6 @@ return [
 		'cache' => [
 			'class' => 'yii\caching\FileCache',
 		],
-		'view' => [
-			'theme' => [
-				'pathMap' => [
-					'@app/views' => '@vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app',
-				],
-			],
-		],
+		
 	],
 ];
