4ca6e8f6cfc3327581603d6de335bb02b4fe3bfc
wumahoo <471155350@qq.com>
2016-08-08 18:05:02 +0800

    add article

---
+++ backend/controllers/ArticleController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * food items subject to share
+ * Created by PhpStorm.
+ * User: wumahoo
+ * Date: 2016/8/8 0008
+ * Time: 下午 5:18
+ */
+
+namespace backend\controllers;
+
+class ArticleController extends Controller{
+
+}
